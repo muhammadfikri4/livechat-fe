7a14809d9e63d96d5619cc8ed7702b7997454ff8
muhammadfikri4 <muhfikriantoaji@gmail.com>
2025-09-20 11:58:59 +0700

    `fix`: build error

--- src/features/_global/views/AuthLayout.tsx
+++ src/features/_global/views/AuthLayout.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { Navbar } from "../components/Navbar";
 
 export const AuthLayout = () => {
   return (
     <>
-      <Navbar />
       <Outlet />
     </>
   );
